78dd2b13afaf368722aa7740dc8b1c87cca910a9
Vanessa Toro <vanetoro84@gmail.com>
2018-07-30 11:42:31 -0400

    added murals and artist resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'artists/index'
+  get 'artists/show'
   get '/', to: 'application#welcome'
-  resources :murals, only: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :murals, only: [:index, :new]
+  resources :artists, only: [:index, :show]
 end
