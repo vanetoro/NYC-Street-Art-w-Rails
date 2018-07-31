70370ab0d84b7e696714bb3f894b92dabdff1236
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 16:59:21 -0400

    set up set_artist and set_mural helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :set_artist, :set_mural
 
   def welcome
 
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def set_mural
+    @mural = Mural.find(params[:id])
   end
 end
