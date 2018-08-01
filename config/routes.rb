79909caf69bbe6f2054d013ef3b813049e2094e3
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 20:12:20 -0400

    routes for neighborhood murals, artist, sessions, users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get 'artists/index'
-  get 'artists/show'
   get '/', to: 'application#welcome'
-  resources :murals, only: [:index, :new, :create]
-  resources :artists, only: [:index, :show]
+  get 'neighborhoods/top_3', to: 'neighborhoods#top3'
+  get 'artists/most_popular', to: 'artists#popular'
+
+  resources :murals, only: [:index, :new, :create, :update]
+  resources :artists, only: [:index, :show, :new, :create, :update]
   resources :neighborhoods, only:[:index, :show]
+  resources :sessions, only: [:new, :create]
+  resources :users, only: [:new, :create]
 
   resources :artists do
-    resources :mural, only: [:show, :new, :edit]
+    resources :murals, only: [:show, :new, :edit, :show]
   end
 end
