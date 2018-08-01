abbd96762b39879e65f47f7df4e0381abe145119
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 20:05:04 -0400

    added validations and methods to order by artist name and most popular by amount of murals

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,18 @@
 class Artist < ApplicationRecord
   has_many :murals
   has_many :neighborhoods, through: :murals
+  validates :name, presence: true
+  validates :artist_name, presence: true
+  validates :bio, presence: true
+
+  def self.all_artists
+    self.all.order(artist_name: :asc)
+  end
+
+  def self.most_popular
+    
+    joins(:murals).group(:artist_id).order('count(murals.artist_id)desc').limit(5)
+  end
+
+
 end
