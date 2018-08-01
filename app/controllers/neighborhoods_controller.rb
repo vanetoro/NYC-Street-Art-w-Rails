e22c60ce27a99ea15f79df7e8a5bd4faf5ec021f
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 20:02:26 -0400

    order hoods by name and created top hoods

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,9 +1,17 @@
 class NeighborhoodsController < ApplicationController
+  layout 'logged_in'
   def index
-    @neighborhoods = Neighborhood.all
+    @neighborhoods = Neighborhood.all_hoods
   end
 
   def show
     @neighborhood = Neighborhood.find(params[:id])
+    binding.pry
   end
+
+  def top3
+    @neighborhoods = Neighborhood.top_hoods
+    render :index
+  end
+
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,4 +1,15 @@
 class Neighborhood < ApplicationRecord
   has_many :murals
   has_many :artists, through: :murals
+  validates :name, presence: true
+
+
+  def self.all_hoods
+    self.all.order(name: :asc)
+  end
+
+  def self.top_hoods
+    joins(:murals).group(:neighborhood_id).order('count(murals.neighborhood_id)desc').limit(3)
+  end
+
 end
