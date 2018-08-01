75a772eb8be4c879eda326b10177198bf40f83fd
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 20:04:29 -0400

    created image method, set_artist_by_id, active and fixed instagram method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,27 @@
 module ApplicationHelper
 
   def instagram(artist)
-    if !artist.instagram.nil?
-      link_to 'Instagram', artist.instagram, :target => "_blank"
+    if !artist.instagram.blank?
+      link_to 'Instagram', "https://www.instagram.com/#{artist.instagram}", :target => "_blank"
     end
   end
 
+  def image(mural)
+    if mural.avatar.attached?
+      image_tag mural.avatar.variant(resize: "50X50")
+    end
+  end
+
+  def set_artist_by_id
+    @artist = Artist.find(params[:artist_id]) || mural.artist_id
+  end
+
+  def active?(mural)
+    if mural.active == 1
+      'Mural is currently on display'
+    else
+      "No longer available"
+    end
+  end
 
 end
