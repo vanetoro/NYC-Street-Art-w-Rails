7b0b9e8eba4001d6ee835de06fee1221f2967a83
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 20:01:03 -0400

    set up logged in layout and added new, update and  popular

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,44 @@
 class ArtistsController < ApplicationController
-
+layout 'logged_in'
   def index
-    @artists =Artist.all
+    @artists =Artist.all_artists
   end
 
   def show
-    @artist = Artist.find(params[:id])
+    set_artist
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to artist_path(@artist)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    set_artist
+  end
+
+  def update
+    set_artist
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
+  def popular
+    @artists = Artist.most_popular
+    render :index
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :artist_name, :instagram, :bio)
   end
 end
