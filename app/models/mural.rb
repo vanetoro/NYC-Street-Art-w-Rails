fbfdde40a6ed292b1342472b0c5cca8266a977d8
Vanessa Toro <vanetoro84@gmail.com>
2018-07-31 20:05:45 -0400

    added validations and pictures helper method

--- app/models/mural.rb
+++ app/models/mural.rb
@@ -1,12 +1,19 @@
 class Mural < ApplicationRecord
   belongs_to :artist
   belongs_to :neighborhood
+  belongs_to :user, optional: true
+  validates :location_details, presence: true
+  validates :neighborhood_id, presence: true
+  has_one_attached :avatar
 
   def neighborhood_attributes=(params)
     if !params[:name].blank?
       neighborhood = Neighborhood.find_or_create_by(name: params[:name].titleize)
       self.neighborhood = neighborhood
     end
-end
+  end
 
+  def self.ordered_by_hood
+    binding.pry
+  end
 end
