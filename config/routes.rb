5fbabd768dd1a0e9a65eab49ca9932b9b0fb22cd
Vanessa Toro <vanetoro84@gmail.com>
2018-08-03 18:36:35 -0400

    added history route

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   get '/', to: 'application#welcome', as: 'root'
+  get '/history', to: 'application#history'
   get 'neighborhoods/top_3', to: 'neighborhoods#top3'
   get 'artists/most_popular', to: 'artists#popular'
   get '/signout', to: 'sessions#destroy', as: 'signout'
-  get 'auth/google_oauth2/callback', to: 'sessions#create'
+  
 
-  resources :murals, only: [:index, :new, :create, :update, :delete, :show]
+
+  resources :murals, only: [:index, :new, :create, :update, :show]
   resources :artists, only: [:index, :show, :new, :create, :update]
   resources :neighborhoods, only:[:index, :show]
   resources :sessions, only: [:new, :create]
   resources :users, only: [:new, :create, :edit, :update]
-
+  get 'auth/google_oauth2/callback', to: 'sessions#create'
   resources :artists do
     resources :murals, only: [:show, :new, :edit, :show]
   end
