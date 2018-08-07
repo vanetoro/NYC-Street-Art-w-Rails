76606456642eea0e0ddaa19215f1664a411da7f9
Vanessa Toro <vanetoro84@gmail.com>
2018-08-06 20:27:00 -0400

    moved sign in methods from applicaction controller to sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,31 +34,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def sign_in_with_password
-    @user = User.find_by(username: params[:user][:username])
-      if !@user.nil? && @user.authenticate(params[:user][:password])
-        session[:user_id] = @user.id
-        redirect_to artists_path
-      else
-        flash[:error] = 'Username or password is incorrect. Please try again.'
-        @user = User.new
-        render :new
-      end
-  end
 
-  def sign_in_with_auth(auth)
-    @user = User.find_by(email: auth[:info][:email])
-      if @user.blank?
-        @user = User.new(email: auth[:info][:email]) do |u|
-          u.username = auth['info']['email'].split('@')[0]
-          u.email = auth['info']['email']
-          u.password = SecureRandom.hex
-          @user.save
-        end
-      end
-      session[:user_id] = @user.id
-      redirect_to artists_path
-  end
 
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,5 +20,34 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
+  private
+
+  def sign_in_with_password
+    @user = User.find_by(username: params[:user][:username])
+      if !@user.nil? && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to artists_path
+      else
+        flash[:error] = 'Username or password is incorrect. Please try again.'
+        @user = User.new
+        render :new
+      end
+  end
+
+  def sign_in_with_auth(auth)
+    @user = User.find_by(email: auth[:info][:email])
+      if @user.nil?
+        @user = User.new(email: auth[:info][:email]) do |u|
+          u.username = auth['info']['email'].split('@')[0]
+          u.email = auth['info']['email']
+          u.password = SecureRandom.hex
+          u.password_confirmation = u.password
+          u.save
+        end
+      end
+      session[:user_id] = @user.id
+      redirect_to artists_path
+  end
+
 
 end
