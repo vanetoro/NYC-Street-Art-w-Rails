fcb6fd7940db04813a11e6ba739616c4c04824b2
Vanessa Toro <vanetoro84@gmail.com>
2018-08-06 20:35:06 -0400

    added routes to murals

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,18 @@ Rails.application.routes.draw do
   get 'neighborhoods/top_3', to: 'neighborhoods#top3'
   get 'artists/most_popular', to: 'artists#popular'
   get '/signout', to: 'sessions#destroy', as: 'signout'
-  
 
 
-  resources :murals, only: [:index, :new, :create, :update, :show]
+
+  resources :murals, only: [:index, :show, :destroy, :update , :new, :create]
   resources :artists, only: [:index, :show, :new, :create, :update]
   resources :neighborhoods, only:[:index, :show]
   resources :sessions, only: [:new, :create]
   resources :users, only: [:new, :create, :edit, :update]
+
   get 'auth/google_oauth2/callback', to: 'sessions#create'
+
   resources :artists do
-    resources :murals, only: [:show, :new, :edit, :show]
+    resources :murals, only: [:show, :new, :edit, :update, :destroy, :create]
   end
 end
