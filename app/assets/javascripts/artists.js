cebfeb6f7c765beaf0fc87983bd097c938476de3
Vanessa Toro <vanetoro84@gmail.com>
2018-08-23 19:48:19 +0000

    refactoring js code and put in assests folder

---
+++ app/assets/javascripts/artists.js
@@ -0,0 +1,31 @@
+// <script type="text/javascript" charset='utf-8'>
+//   $(function() {
+//     $("#newMural").on('click', function(e) {
+//       e.preventDefault()
+//       $("#newMuralForm").css({
+//         'display': 'block',
+//     })
+//   })
+//   })
+// </script>
+
+$(document).ready(function() {
+  attachListeners();
+});
+
+function attachListeners(){
+  debugger
+  $("#newMural").on('click', (e) => displayNewMuralForm(e))
+}
+
+
+function displayBlock(element){
+  $(element).css({
+    'display': 'block'
+  })
+}
+
+function displayNewMuralForm(e) {
+    e.preventDefault()
+    displayBlock($("#newMuralForm"))
+}
