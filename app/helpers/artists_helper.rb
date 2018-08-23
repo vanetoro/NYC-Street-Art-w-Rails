9206fc34e57fa95ac9b03dda255bfb6d85ec2466
Vanessa Toro <vanetoro84@gmail.com>
2018-08-23 17:54:13 +0000

    Fixed issue with next and previous buttons breaking because of no id found

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,3 +1,19 @@
 module ArtistsHelper
 
+
+  def next_mural(mural)
+    if mural.next
+      link_to 'Next', artist_mural_path(@mural.artist, @mural.next) , class: 'nextMural'
+    else
+      link_to 'Next'
+    end
+  end
+
+  def previous_mural(mural)
+    if !mural.previous.nil?
+      link_to 'Previous', artist_mural_path(@mural.artist, @mural.previous), class: 'nextMural'
+    else
+      link_to 'Previous'
+    end
+  end
 end
