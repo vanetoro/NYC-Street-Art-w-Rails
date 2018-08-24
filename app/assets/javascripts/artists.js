d1835fffa05760b95c86ab75df6e60be46379d8a
Vanessa Toro <vanetoro84@gmail.com>
2018-08-24 14:50:38 -0400

    js refactoring

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -9,23 +9,23 @@
 //   })
 // </script>
 
-$(document).ready(function() {
-  attachListeners();
-});
-
-function attachListeners(){
-  debugger
-  $("#newMural").on('click', (e) => displayNewMuralForm(e))
-}
-
-
-function displayBlock(element){
-  $(element).css({
-    'display': 'block'
-  })
-}
-
-function displayNewMuralForm(e) {
-    e.preventDefault()
-    displayBlock($("#newMuralForm"))
-}
+// $(document).ready(function() {
+//   attachListeners();
+// });
+//
+// function attachListeners(){
+//   debugger
+//   $("#newMural").on('click', (e) => displayNewMuralForm(e))
+// }
+//
+//
+// function displayBlock(element){
+//   $(element).css({
+//     'display': 'block'
+//   })
+// }
+//
+// function displayNewMuralForm(e) {
+//     e.preventDefault()
+//     displayBlock($("#newMuralForm"))
+// }
