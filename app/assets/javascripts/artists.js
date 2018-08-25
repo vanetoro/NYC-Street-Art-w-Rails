f502b8ba720a5a1fbbe37032dc1877532fd44067
Vanessa Toro <vanetoro84@gmail.com>
2018-08-24 20:00:07 -0400

    created document.ready, attachedListeners,displayBlock & diplayNewMuralForm functions

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -1,31 +1,40 @@
-// <script type="text/javascript" charset='utf-8'>
-//   $(function() {
-//     $("#newMural").on('click', function(e) {
-//       e.preventDefault()
-//       $("#newMuralForm").css({
-//         'display': 'block',
-//     })
-//   })
-//   })
-// </script>
+$(document).ready(function() {
+  attachListeners();
+});
 
-// $(document).ready(function() {
-//   attachListeners();
-// });
-//
-// function attachListeners(){
-//   debugger
-//   $("#newMural").on('click', (e) => displayNewMuralForm(e))
-// }
-//
-//
-// function displayBlock(element){
-//   $(element).css({
-//     'display': 'block'
-//   })
-// }
-//
-// function displayNewMuralForm(e) {
-//     e.preventDefault()
-//     displayBlock($("#newMuralForm"))
-// }
+function attachListeners(){
+  $("#newMural").on('click', (e) => displayNewMuralForm(e))
+  $("a.nextMural").on('click', (e) => nextMural(e))
+  $("form#new_mural").on('submit', (e) => createMural(e))
+}
+
+function displayBlock(element){
+  $(element).css({
+    'display': 'block'
+  })
+}
+
+function displayNewMuralForm(e) {
+    e.preventDefault()
+    displayBlock($("#newMuralForm"))
+}
+
+function nextMural(e){
+  e.preventDefault()
+  let path = e['target']['pathname']
+  let nextMural = $.getJSON(path)
+  nextMural.done(function(mural) {
+  $("#muralLocation").text(mural.location_details)
+  $("#muralHood").html(`<a data-id="${mural.neighborhood.id}" href="${mural.neighborhood.name}"> ${mural.neighborhood.name}</a>`)
+  })
+
+}
+
+function createMural(e){
+  event.preventDefault();
+  var values = $(this).serialize()
+  var posting = $.post('/murals', values)
+  posting.done(function (data){
+    debugger
+  })
+}
