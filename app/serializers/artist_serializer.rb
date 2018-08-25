aa12ac779e3c3f39bad49ba0a7556bb2c42a4fc1
Vanessa Toro <vanetoro84@gmail.com>
2018-08-25 12:36:23 -0400

    added artist info to update show page when clicking next

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,5 +1,5 @@
 class ArtistSerializer < ActiveModel::Serializer
-  attributes :id, :artist_name
+  attributes :id, :artist_name, :name, :bio, :instagram
   has_many :murals
   # has_many :neighborhoods, through: :murals
 end
