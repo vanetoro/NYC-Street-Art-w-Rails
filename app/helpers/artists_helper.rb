9b07418b0a0aad6917dc9512a3360e7d33a2d73c
Vanessa Toro <vanetoro84@gmail.com>
2018-08-25 15:10:28 -0400

    moved instagram and show_next_artist methods to artist helper

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,17 @@
 module ArtistsHelper
 
+  def instagram(artist)
+    if !artist.instagram.blank?
+      link_to 'Instagram', "https://www.instagram.com/#{artist.instagram}", :target => "_blank", id: 'instagram'
+    end
+  end
 
+  def show_next_artist(artist)
+    if !artist.nil?
+      content_tag(:a, 'Next', class: "pointer", id: "nextArtist", data:{id: @nextArtist.id})
+    else
+      content_tag(:a, 'Previous', class: "pointer", id: "previousArtist", data:{id: @previousArtist.id})
+    end
+  end
 
 end
