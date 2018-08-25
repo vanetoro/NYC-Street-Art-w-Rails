0e3e18ff385f97de293a10ad0a41b751c069d409
Vanessa Toro <vanetoro84@gmail.com>
2018-08-24 20:02:21 -0400

    moved next and previous mural from artist helper to mural helper

--- app/helpers/murals_helper.rb
+++ app/helpers/murals_helper.rb
@@ -15,5 +15,25 @@ module MuralsHelper
   end
 
 
+  def next_mural(mural)
+    if mural.next
+      link_to "More by #{@mural.artist.artist_name}" , artist_mural_path(@mural.artist, @mural.next) , class: 'nextMural'
+    else
+      link_to 'Next'
+    end
+  end
 
+  def previous_mural(mural)
+    if !mural.previous.nil?
+      link_to 'Previous', artist_mural_path(@mural.artist, @mural.previous), class: 'nextMural'
+    else
+      tag.a 'Previous', class: 'nextMural'
+    end
+  end
+
+  def nearby_murals(nearby)
+    nearby.murals.map do |mural|
+      mural.location_details
+    end
+  end
 end
