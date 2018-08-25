d6e746e52a10819689f9a2bae051a6a4a528043f
Vanessa Toro <vanetoro84@gmail.com>
2018-08-25 12:31:35 -0400

    helper to makes sure page doesn't crash if no more artists to display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,15 @@ module ApplicationHelper
 
   def instagram(artist)
     if !artist.instagram.blank?
-      link_to 'Instagram', "https://www.instagram.com/#{artist.instagram}", :target => "_blank"
+      link_to 'Instagram', "https://www.instagram.com/#{artist.instagram}", :target => "_blank", id: 'instagram'
     end
   end
 
+  def show_next_artist(artist)
+    if !artist.nil?
+      link_to 'Next', artist_path(@nextArtist), class: "nextArtist", data:{id: @nextArtist.id}
+    end
+  end
 
   def set_artist_by_id
     @artist = Artist.find(params[:artist_id]) || mural.artist_id
