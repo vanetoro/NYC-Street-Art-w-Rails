75a28b177f384278195e5898a6bd74214faaa83f
Vanessa Toro <vanetoro84@gmail.com>
2018-08-28 19:38:09 -0400

    commented out show_next_artist

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -6,12 +6,12 @@ module ArtistsHelper
     end
   end
 
-  def show_next_artist(artist)
-    if !artist.nil?
-      content_tag(:a, 'Next', class: "pointer", id: "nextArtist", data:{id: @nextArtist.id})
-    else
-      content_tag(:a, 'Previous', class: "pointer", id: "previousArtist", data:{id: @previousArtist.id})
-    end
-  end
+  # def show_next_artist(artist)
+  #   if !artist.nil?
+  #     content_tag(:a, 'Next', class: "pointer", id: "nextArtist", data:{id: @nextArtist.id})
+  #   else
+  #     content_tag(:a, 'Previous', class: "pointer", id: "previousArtist", data:{id: @previousArtist.id})
+  #   end
+  # end
 
 end
